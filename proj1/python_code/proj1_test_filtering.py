59df4b89933e3673331af05d2a06a2e3143b900d
“LuoCJ” <1979707287@qq.com>
2019-05-11 10:32:10 +0800

    基本完成了滤波操作

--- proj1/python_code/proj1_test_filtering.py
+++ proj1/python_code/proj1_test_filtering.py
@@ -11,5 +11,5 @@ identity_filter = np.array(([0, 0, 0], [0, 1, 0], [0, 0, 0]), dtype="float32")
 identity_image = my_imfilter(test_image, identity_filter)
 identity_image2 = vis_hybrid_image(test_image)
 
-cv2.imshow("", identity_image2)
+cv2.imshow("", identity_image)
 cv2.waitKey(0)
