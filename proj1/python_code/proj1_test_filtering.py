5a7b883e8531fc4864278740133664605ec1fbd5
“LuoCJ” <1979707287@qq.com>
2019-05-12 16:54:25 +0800

    完成了test部分的代码

--- proj1/python_code/proj1_test_filtering.py
+++ proj1/python_code/proj1_test_filtering.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from my_imfilter import my_imfilter
-from utils import luo_imshow, luo_imwrite
+from utils import luo_imshow, luo_imwrite, luo_fspecial
 from vis_hybrid_image import vis_hybrid_image
 
 # this script has test cases to help you test my_imfilter() which you will
@@ -15,23 +15,72 @@ cv2.destroyAllWindows()
 ## Setup
 test_image = cv2.imread("../data/cat.bmp")
 test_image = cv2.resize(test_image, (0, 0), fx=0.7, fy=0.7)
+
+
 # luo_imwrite(test_image, "Figure 1")
 
 ## Identify filter
 # This filter should do nothing regardless of the padding method you use.
-identity_filter = np.array(([0, 0, 0], [0, 1, 0], [0, 0, 0]), dtype="float32")
-identity_image = my_imfilter(test_image, identity_filter)
-# luo_imshow("Figure 2", identity_image)
-luo_imwrite(identity_image, 'identity_image.jpg')
+def identity():
+    identity_filter = np.array(([0, 0, 0], [0, 1, 0], [0, 0, 0]), dtype="float32")
+    identity_image = my_imfilter(test_image, identity_filter)
+    luo_imshow("Figure 2", identity_image)
+    luo_imwrite(identity_image, 'identity_image.jpg')
+    return identity_image
+
 
 ## Small blur with a box filter
 # This filter should remove some high frequencies
-blur_filter = np.array(([1, 1, 1], [1, 1, 1], [1, 1, 1]), dtype="float32")
-blur_filter = blur_filter / np.sum(blur_filter)
-blur_image = my_imfilter(test_image, blur_filter)
-# luo_imshow("Figure 3", blur_image)
-luo_imwrite(identity_image, 'blur_image.jpg')
+def blur():
+    blur_filter = np.array(([1, 1, 1], [1, 1, 1], [1, 1, 1]), dtype="float32")
+    blur_filter = blur_filter / np.sum(blur_filter)
+    blur_image = my_imfilter(test_image, blur_filter)
+    luo_imshow("Figure 3", blur_image)
+    luo_imwrite(blur_image, 'blur_image.jpg')
+    return blur_image
+
 
 ## Large blur
 # This blur would be slow to do directly, so we instead use the fact that
 # %Gaussian blurs are separable and blur sequentially in each direction.
+
+def large_1d_blur():
+    large_1d_blur_filter = luo_fspecial(25, 1, 10)
+    large_blur_image = my_imfilter(test_image, large_1d_blur_filter)
+    large_blur_image = my_imfilter(large_blur_image, large_1d_blur_filter)
+    luo_imshow("Figure 4", large_blur_image)
+    luo_imwrite(large_blur_image, 'large_blur_image.jpg')
+    return large_blur_image
+
+
+def sobel():
+    sobel_filter = np.array(([-1, 0, 1], [-2, 0, 2], [-1, 0, 1]), dtype="float32")
+    sobel_image = my_imfilter(test_image, sobel_filter)
+    luo_imshow("Figure 5", sobel_image + 0.5)
+    luo_imwrite(sobel_image + 0.5, "sobel_image.jpg")
+    return sobel_image
+
+
+def laplacian():
+    laplacian_filter = np.array(([0, 1, 0], [1, -4, 1], [0, 1, 0]), dtype="float32")
+    laplacian_image = my_imfilter(test_image, laplacian_filter)
+    luo_imshow("Figure 6", laplacian_image + 0.5)
+    luo_imwrite(laplacian_image + 0.5, "laplacian_image.jpg")
+    return laplacian_image
+
+
+def high_pass():
+    high_pass_image = test_image - blur()
+    luo_imshow("Figure 7", high_pass_image + 0.5)
+    luo_imwrite(high_pass_image + 0.5, "high_pass_image.jpg")
+    return high_pass_image
+
+
+if __name__ == "__main__":
+    # identity()
+    # blur()
+    # large_1d_blur()
+    # sobel()
+    laplacian()
+    # high_pass()
+    pass
